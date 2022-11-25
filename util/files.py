01f87442b827d879280bbb4bb0c9dcc5af206e18
nvb-uy <almhak@proton.me>
2022-11-25 03:23:38 -0600

    + Templates

--- util/files.py
+++ util/files.py
@@ -1,12 +1,3 @@
-# Common files that both loaders use.
-common = [
-    "gradlew",
-    "gradlew.bat",
-    "gradle/wrapper/gradle-wrapper.properties",
-    "gradle/wrapper/gradle-wrapper.jar",
-    ".github/workflows/build.yml"
-]
-
 # List of files that need placeholder replacement.
 class placeholder:
     fabric = [
